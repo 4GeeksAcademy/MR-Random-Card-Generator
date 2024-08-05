0e9b46828ac7f089d8ef8c5309232a3dd8d1c5e3
MicaelaRojas <67761871+MicaelaRojas@users.noreply.github.com>
2024-08-05 16:34:32 +0000

    Random Card Generator V.1

--- src/app.js
+++ src/app.js
@@ -6,6 +6,35 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  genCard();
+  var button = document.getElementById("boton");
+  button.addEventListener("click", genCard);
 };
+
+function genCard() {
+  let values = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "K",
+    "Q"
+  ];
+  let suits = ["♦", "♥", "♠", "♣"];
+  let suitNames = ["diamond", "heart", "spade", "club"];
+
+  let randomValue = values[Math.floor(Math.random() * values.length)];
+  let randomSuitIndex = Math.floor(Math.random() * suits.length);
+  let randomSuit = suits[randomSuitIndex];
+  let suitName = suitNames[randomSuitIndex];
+
+  let cardElement = document.getElementById("card");
+  cardElement.innerHTML = `<span class="card ${suitName}">${randomValue} ${randomSuit}</span>`;
+}
